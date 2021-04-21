5eadadd86cc987aeeacfcc8e9eab03a27fef6501
Alexey Rochev <equeim@gmail.com>
2021-04-21 21:01:43 +0300

    Update dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,5 @@
 pluginManagement {
-    val kotlin = "1.4.30"
+    val kotlin = "1.4.32"
     plugins {
         kotlin("multiplatform") version(kotlin)
         kotlin("plugin.serialization") version(kotlin)
