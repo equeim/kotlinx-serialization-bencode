7c50c5f5f077b41a0e6c2cbb40a1f1ee0108312c
Alexey Rochev <equeim@gmail.com>
2021-05-22 00:00:39 +0300

    Remove serialization plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,21 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm")
-    kotlin("plugin.serialization")
+    kotlin("jvm") version("1.5.0")
 }
 
 group = "org.equeim"
 version = "0.1"
-
-repositories {
-    mavenCentral()
-}
 
 tasks.named<KotlinCompile>("compileKotlin") {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_1_8.toString()
         freeCompilerArgs = freeCompilerArgs + "-Xopt-in=kotlinx.serialization.ExperimentalSerializationApi"
     }
+}
+
+repositories {
+    mavenCentral()
 }
 
 dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,1 @@
-pluginManagement {
-    val kotlin = "1.5.0"
-    plugins {
-        kotlin("jvm") version(kotlin)
-        kotlin("plugin.serialization") version(kotlin)
-    }
-}
-
 rootProject.name = "kotlinx-serialization-bencode"
