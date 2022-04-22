d758c0df2b97f8fd5d74b41e013ccd10b999527b
Alexey Rochev <equeim@gmail.com>
2022-04-22 22:41:04 +0300

    Add com.github.ben-manes.versions plugin and update Kotlin to 1.6.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,10 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.util.*
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.gradle.versions.plugin)
 }
 
 group = "org.equeim"
@@ -22,4 +25,23 @@ repositories {
 dependencies {
     api(libs.serialization.core)
     api(libs.coroutines.core)
+}
+
+tasks.named<DependencyUpdatesTask>("dependencyUpdates") {
+    val checker = DependencyVersionChecker()
+    rejectVersionIf {
+        checker.isNonStable(candidate.version)
+    }
+}
+
+class DependencyVersionChecker {
+    private val stableKeywords = listOf("RELEASE", "FINAL", "GA")
+    private val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+
+    fun isNonStable(version: String): Boolean {
+        val versionUppercase = version.toUpperCase(Locale.ROOT)
+        val hasStableKeyword = stableKeywords.any(versionUppercase::contains)
+        val isStable = hasStableKeyword || regex.matches(version)
+        return isStable.not()
+    }
 }
